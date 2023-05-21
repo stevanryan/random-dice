7fd98d81990433666de26dc8f11b9954bee084de
stevanryan <stevanryan85@gmail.com>
2023-05-21 14:46:15 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import DiceContext from './Components/DiceContext';
+import Navigation from './Components/Navigation';
+import Home from './Components/Home';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 function App() {
+  const client = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+      }
+    }
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <QueryClientProvider client={client}>
+        <DiceContext>
+          <Navigation />
+          <Home />
+        </DiceContext>
+      </QueryClientProvider>
     </div>
   );
 }
